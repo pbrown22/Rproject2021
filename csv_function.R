04fed07af9f0baafd239d97c19e7655484ec4b93
Zack Tabellione <ztabelli@nd.edu>
2021-12-01 11:09:07 -0500

    first support function

--- csv_function.R
+++ csv_function.R
@@ -1,5 +1,5 @@
-cmdlfiles<-function(input){
-  y<-getwd()
+cmdlfiles<-function(directory){
+  y<-setwd(./directory)
   x<-list.files(path = y, pattern = "*.txt")
  
    for(i in x){
@@ -8,11 +8,10 @@ cmdlfiles<-function(input){
    b<-gsub(pattern = "\\.txt$","",i)
    c<-assign(b,paste(b,".csv",sep = ""))
    write.csv(a, file = c, quote = FALSE)
-   
-  }
+   }
+}
   
-  list.files(path = ".", pattern = "*.txt")
-  path.expand("./countryY")
-  screen_120.csv
+  
+  
   
   
